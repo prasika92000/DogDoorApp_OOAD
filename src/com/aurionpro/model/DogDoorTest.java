c0b0c6610ecf21f8a871b68567dabfd1be4d00ad
Rasika~ <rasikabhai.patil@aurionpro.com~>
2023-08-28 22:54:03 +0530

    Dog Door App

---
+++ src/com/aurionpro/model/DogDoorTest.java
@@ -0,0 +1,22 @@
+package com.aurionpro.model;
+
+public class DogDoorTest {
+
+	public static void main(String[] args) {
+		  DogDoor door = new DogDoor();
+	        Remote remote = new Remote(door);
+
+	        System.out.println("Fido barks to go outside...");
+	        remote.pressButton();
+
+	        System.out.println("\n Fido has gone outside...");
+	        remote.pressButton();
+	        
+	        System.out.println("\n Fido's all done...");
+	        remote.pressButton();
+	        
+	        System.out.println(" \n Fido's back inside...");
+	        remote.pressButton();
+	}
+
+}
